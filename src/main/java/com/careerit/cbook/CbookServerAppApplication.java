cb542b37e0c326444ebe8b5d29d881a3993536e0
javawithlwl <javawithlwl@gmail.com>
2024-04-17 07:57:41 +0530

    Added docker configurations

--- src/main/java/com/careerit/cbook/CbookServerAppApplication.java
+++ src/main/java/com/careerit/cbook/CbookServerAppApplication.java
@@ -1,13 +1,24 @@
 package com.careerit.cbook;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CbookServerAppApplication {
+public class CbookServerAppApplication  {
 
+	@Value("${greetings}")
+	private String greetings;
 	public static void main(String[] args) {
 		SpringApplication.run(CbookServerAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args ->{
+			System.out.println("Greetings : "+greetings);
+		};
+	}
 }
